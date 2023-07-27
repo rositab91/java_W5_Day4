d9141a1be515f15f337e5ecd012855afb0080b7b
rositab91 <rositab91@live.it>
2023-07-27 16:58:30 +0200

    java_Week5_Day4

---
+++ src/main/java/rositabongiovanni/java_W5_Day4/JavaW5Day4Application.java
@@ -0,0 +1,13 @@
+package rositabongiovanni.java_W5_Day4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaW5Day4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaW5Day4Application.class, args);
+	}
+
+}
---
+++ src/test/java/rositabongiovanni/java_W5_Day4/JavaW5Day4ApplicationTests.java
@@ -0,0 +1,13 @@
+package rositabongiovanni.java_W5_Day4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaW5Day4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
